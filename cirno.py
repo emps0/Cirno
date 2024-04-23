0f7552c786bc69803e39ce39c32646db7ef523cf
emps0 <144963088+emps0@users.noreply.github.com>
2024-04-22 23:28:51 -0500

    Update cirno.py

--- cirno.py
+++ cirno.py
@@ -23,9 +23,16 @@ for line in lines:
         if line == instr:
             print(brain.pop())
 
-        elif line.startswith("say '") and line[-1]:
+        elif line.startswith("say '") and line[-1] == "'":
             message = line[5:-1] # need to exclude the last '
             print(message)
+        
+        elif line.startswith("say '") and line[-1] == "x":
+            closing_quote_index = line[5:].find("'")
+            message = line[5:5+closing_quote_index] # need to exclude the last '
+            times_to_print = int(line[5+closing_quote_index+1:-1])
+            for i in range(times_to_print):
+                print(message)
             
         else:
             raise SyntaxError
@@ -55,3 +62,14 @@ for line in lines:
         for i in range(times_to_multiply):
             result *= brain.pop()
         brain.append(result)
+
+    elif instr.lower() == "reverse":
+        if line == instr:
+            original_text = brain.pop()
+            reversed_text = original_text[::-1]
+            print(reversed_text)
+
+        elif line.startswith("reverse '") and line[-1] == "'":
+            message = line[9:-1] 
+            reversed_message = message[::-1]  
+            print(reversed_message)
